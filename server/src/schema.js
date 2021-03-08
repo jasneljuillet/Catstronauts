04a1a74f6a07543411f4bab7ba3a58b97350d68e
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-08 12:49:37 -0500

    Catstronautst App

---
+++ server/src/schema.js
@@ -0,0 +1,30 @@
+const { gql } = require("apollo-server");
+
+const TypeDefs = gql `
+
+    " Query for get Tracks array for the home page "
+    type Query {
+        tracksForHome: [Track!]
+    }
+
+    " a Track is a group of modules that teaches about a specific topic"
+
+    type Track {
+        id: ID!
+        title: String!
+        author: String!
+        thumbnail: String
+        length: Int
+        ModulesCount: Int
+    }
+
+    " Author of a complete Track "
+
+    type Author {
+        id: Id!
+        name: String!
+        picture: String
+    }
+`;
+
+module.exports = TypeDefs;
